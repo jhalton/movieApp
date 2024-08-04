35b32162ddab73bf7b0b7c8840e6ddcf90a15c58
Jeanette <haltonjeanette@gmail.com>
2024-08-04 13:11:42 -0700

    Movie Trend card

--- movie-app/src/components/movie-card/movie-trend.tsx
+++ movie-app/src/components/movie-card/movie-trend.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import { MovieDataType } from "../../assets/data";
+import { MovieContext } from "../../context/movie-context";
+import { Card, CardContent } from "@mui/material";
 
 interface MovieTrendCardProps {
   movie: MovieDataType;
 }
 
 const MovieTrendCard = ({ movie }: MovieTrendCardProps) => {
-  return <div>MovieTrendCard</div>;
+  const { dispatch } = useContext(MovieContext);
+  return (
+    <Card
+      key={movie.id}
+      elevation={0}
+      style={{ backgroundColor: "transparent" }}
+    >
+      <CardContent
+        style={{
+          padding: 0,
+          position: "relative",
+          overflowX: "scroll",
+          display: "flex",
+        }}
+      >
+        image
+      </CardContent>
+    </Card>
+  );
 };
 
 export default MovieTrendCard;
