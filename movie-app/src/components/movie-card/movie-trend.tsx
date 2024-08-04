46a2c35388eab445eb3dd9fa58d42986fd237dab
Jeanette <haltonjeanette@gmail.com>
2024-08-04 13:38:24 -0700

    Create MovieTrendCard Component

--- movie-app/src/components/movie-card/movie-trend.tsx
+++ movie-app/src/components/movie-card/movie-trend.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { MovieDataType } from "../../assets/data";
 import { MovieContext } from "../../context/movie-context";
-import { Card, CardContent } from "@mui/material";
+import { Box, Card, CardContent, Grid, Stack, Typography } from "@mui/material";
 
 interface MovieTrendCardProps {
   movie: MovieDataType;
@@ -23,7 +23,96 @@ const MovieTrendCard = ({ movie }: MovieTrendCardProps) => {
           display: "flex",
         }}
       >
-        image
+        <span style={{ width: "300px", height: "100%", borderRadius: "8px" }}>
+          image
+        </span>
+        <Box
+          position="absolute"
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          bgcolor="rgba(0, 0, 0, 0, 0.6)"
+          borderRadius="8px"
+        />
+        <Stack
+          mt="6"
+          spacing={0}
+          position="absolute"
+          bottom={0}
+          left={0}
+          right={0}
+          p={4}
+        >
+          <Grid container alignItems="center" spacing={1}>
+            <Grid item>
+              <Typography
+                fontSize={10}
+                color="E0E0E0"
+                aria-label="year-of-movie"
+              >
+                {movie.year}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Box
+                sx={{
+                  width: "1rem",
+                  height: "1rem",
+                  background: "E0E0E0",
+                  borderRadius: "full",
+                }}
+              ></Box>
+            </Grid>
+            <Grid item>
+              <span style={{ width: 16, height: 16, borderRadius: "8px" }}>
+                image
+              </span>
+            </Grid>
+            <Grid item>
+              <Typography
+                fontSize={10}
+                color="E0E0E0"
+                aria-label="movie category"
+              >
+                {movie.category}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Box
+                sx={{
+                  width: "1rem",
+                  height: "1rem",
+                  background: "E0E0E0",
+                  borderRadius: "full",
+                }}
+              ></Box>
+            </Grid>
+            <Grid item>
+              <Typography
+                fontSize={10}
+                color="E0E0E0"
+                aria-label="movie rating"
+              >
+                {movie.rating}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Typography color="E0E0E0" padding={0} aria-label="movie title">
+            {movie.title}
+          </Typography>
+        </Stack>
+        <Box
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            display: "flex",
+            justifyContent: "flex-end",
+            padding: "16px",
+          }}
+        ></Box>
       </CardContent>
     </Card>
   );
