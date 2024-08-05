1855b5beb57a640b77aea3d9b5cac46cc2fd94f5
Jeanette <haltonjeanette@gmail.com>
2024-08-04 17:35:01 -0700

    Create Movie Card Component

--- movie-app/src/components/movie-card/index.tsx
+++ movie-app/src/components/movie-card/index.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { MovieDataType } from "../../assets/data";
+import { MovieContext } from "../../context/movie-context";
+import { Box, Card, CardContent, Grid, Stack, Typography } from "@mui/material";
 
 interface MovieCardProps {
   movie: MovieDataType;
 }
 
 const MovieCard = ({ movie }: MovieCardProps) => {
-  return <div>MovieCard</div>;
+  const { dispatch } = useContext(MovieContext);
+
+  const handleToggleBookmark = (id: string) => {
+    dispatch({ type: "TOGGLE BOOKMARK", id });
+  };
+  return (
+    <Card
+      variant="outlined"
+      sx={{ bgcolor: "transparent", color: "#E0E0E0", my: 3, border: "none" }}
+    >
+      <CardContent sx={{ p: 0, position: "relative" }}>
+        <Grid container spacing={1}>
+          <Grid item>image</Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default MovieCard;
